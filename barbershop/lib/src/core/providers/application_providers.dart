5db19667cd45135e0b82fe20ea04bf792816b624
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-15 19:53:16 -0300

    implementando RestClient/ adicionando Dio/ criadoApplication Providers

---
+++ barbershop/lib/src/core/providers/application_providers.dart
@@ -0,0 +1,7 @@
+import 'package:barbershop/src/core/restClient/rest_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'application_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+RestClient restClient(RestClient ref)=> RestClient();
---
+++ barbershop/lib/src/core/providers/application_providers.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'application_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$restClientHash() => r'd4b66c42ce7838c3b2f036731c9b277b99c07cac';
+
+/// See also [restClient].
+@ProviderFor(restClient)
+final restClientProvider = Provider<RestClient>.internal(
+  restClient,
+  name: r'restClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$restClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RestClientRef = ProviderRef<RestClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
