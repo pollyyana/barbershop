fcb8a6a6dba90f1264acfc05491932b569e09164
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-16 02:20:04 -0300

    refactory Constants de imagens

--- barbershop/lib/src/features/auth/login/login_page.dart
+++ barbershop/lib/src/features/auth/login/login_page.dart
@@ -12,7 +12,7 @@ class LoginPage extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage(
-                'assets/images/background_image_chair.jpg',
+                ImageConstants.backgroudChair,
               ),
               opacity: 0.2,
               fit: BoxFit.cover),
@@ -29,7 +29,7 @@ class LoginPage extends StatelessWidget {
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Image.asset('assets/images/imgLogo.png'),
+                        Image.asset(ImageConstants.imageLogo),
                         const SizedBox(
                           height: 24,
                         ),
--- barbershop/lib/src/features/splash/splash_page.dart
+++ barbershop/lib/src/features/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:barbershop/src/core/ui/constants.dart';
 import 'package:barbershop/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -34,7 +35,7 @@ class _SplashPageState extends State<SplashPage> {
         decoration: const BoxDecoration(
           image: DecorationImage(
               image: AssetImage(
-                'assets/images/background_image_chair.jpg',
+                ImageConstants.backgroudChair,
               ),
               opacity: 0.2,
               fit: BoxFit.cover),
@@ -72,7 +73,9 @@ class _SplashPageState extends State<SplashPage> {
               width: _logoAnimationWidth,
               height: _logoAnimationHeight,
               curve: Curves.linearToEaseOut,
-              child: Image.asset('assets/images/imgLogo.png', fit: BoxFit.cover,),
+              child: Image.asset(
+                ImageConstants.imageLogo, 
+              fit: BoxFit.cover,),
             ),
           ),
         ),
