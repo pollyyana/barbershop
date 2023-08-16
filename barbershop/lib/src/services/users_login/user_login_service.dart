8de47535b97b852b2a8907155a2844699418b09b
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-16 00:59:14 -0300

    User Login Service/SharedPreferences

---
+++ barbershop/lib/src/services/users_login/user_login_service.dart
@@ -0,0 +1,7 @@
+import 'package:barbershop/src/core/exceptions/service_exception.dart';
+import 'package:barbershop/src/core/fp/either.dart';
+import 'package:barbershop/src/core/fp/nil.dart';
+
+abstract interface class UserLoginService {
+Future<Either<ServiceException, Nil>> execute(String email,String password);
+}
