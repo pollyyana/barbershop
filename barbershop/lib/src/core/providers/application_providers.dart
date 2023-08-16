f09baa49018f0f50d5e79812c15c3f87c3fd5359
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-16 20:03:33 -0300

    provider

--- barbershop/lib/src/core/providers/application_providers.dart
+++ barbershop/lib/src/core/providers/application_providers.dart
@@ -1,3 +1,4 @@
+import 'package:barbershop/src/core/fp/either.dart';
 import 'package:barbershop/src/core/restClient/rest_client.dart';
 import 'package:barbershop/src/repositories/user/user_repository.dart';
 import 'package:barbershop/src/repositories/user/user_repository_impl.dart';
@@ -5,17 +6,29 @@ import 'package:barbershop/src/services/users_login/user_login_service.dart';
 import 'package:barbershop/src/services/users_login/user_login_service_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../model/user_model.dart';
+
 part 'application_providers.g.dart';
 
 @Riverpod(keepAlive: true)
-
-RestClient restClient(RestClientRef ref)=> RestClient();
-
-@Riverpod(keepAlive: true)
-UserRepository userRepository(UserRepositoryRef ref) => 
-UserRepositoryImpl(restClient: ref.read(restClientProvider));
+RestClient restClient(RestClientRef ref) => RestClient();
 
 @Riverpod(keepAlive: true)
-UserLoginService userLoginService(UserLoginServiceRef ref) => 
-UserLoginServiceImpl(userRepository:ref.read(userRepositoryProvider) );
+UserRepository userRepository(UserRepositoryRef ref) =>
+    UserRepositoryImpl(restClient: ref.read(restClientProvider));
 
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) =>
+    UserLoginServiceImpl(userRepository: ref.read(userRepositoryProvider));
+
+@Riverpod(keepAlive: true)
+Future<UserModel> getMe(GetMeRef ref) async {
+  final result = await ref.watch(userRepositoryProvider).me();
+
+ return switch (result) {
+    Success(value: final userModel) => userModel,
+    Failure(:final exception) => throw exception,
+
+     
+  };
+}
