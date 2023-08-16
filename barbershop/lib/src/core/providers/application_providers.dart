c28645aec52154c3ce610339a769cdf252e59fbe
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-16 02:26:53 -0300

    refactory user login service provider

--- barbershop/lib/src/core/providers/application_providers.dart
+++ barbershop/lib/src/core/providers/application_providers.dart
@@ -1,6 +1,8 @@
 import 'package:barbershop/src/core/restClient/rest_client.dart';
 import 'package:barbershop/src/repositories/user/user_repository.dart';
 import 'package:barbershop/src/repositories/user/user_repository_impl.dart';
+import 'package:barbershop/src/services/users_login/user_login_service.dart';
+import 'package:barbershop/src/services/users_login/user_login_service_impl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'application_providers.g.dart';
@@ -12,3 +14,8 @@ RestClient restClient(RestClientRef ref)=> RestClient();
 @Riverpod(keepAlive: true)
 UserRepository userRepository(UserRepositoryRef ref) => 
 UserRepositoryImpl(restClient: ref.read(restClientProvider));
+
+@Riverpod(keepAlive: true)
+UserLoginService userLoginService(UserLoginServiceRef ref) => 
+UserLoginServiceImpl(userRepository:ref.read(userRepositoryProvider) );
+
