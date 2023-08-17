ffc20c2d43d3ed06d3aac144fe1d65f33eff12f8
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-17 17:00:24 -0300

    implentado UserRegisterService

---
+++ barbershop/lib/src/services/service_register/user_register_service.dart
@@ -0,0 +1,12 @@
+import '../../core/exceptions/service_exception.dart';
+import '../../core/fp/either.dart';
+import '../../core/fp/nil.dart';
+
+abstract interface class UserRegisterService {
+  Future<Either<ServiceException, Nil>> execute(
+      ({
+        String name,
+        String email,
+        String password,
+      }) userData);
+}
---
+++ barbershop/lib/src/services/service_register/user_register_service_impl.dart
@@ -0,0 +1,30 @@
+import 'package:barbershop/src/core/exceptions/service_exception.dart';
+
+import 'package:barbershop/src/core/fp/either.dart';
+
+import 'package:barbershop/src/core/fp/nil.dart';
+import 'package:barbershop/src/repositories/user/user_repository.dart';
+import 'package:barbershop/src/services/users_login/user_login_service.dart';
+
+import './user_register_service.dart';
+
+class UserRegisterServiceImpl implements UserRegisterService {
+  final UserRepository userRepository;
+  final UserLoginService userLoginService;
+
+  UserRegisterServiceImpl(
+      {required this.userRepository, required this.userLoginService});
+
+  @override
+  Future<Either<ServiceException, Nil>> execute(
+      ({String email, String name, String password}) userData) async {
+    final registerResult = await userRepository.registerAdmin(userData);
+
+    switch (registerResult) {
+      case Success():
+        return userLoginService.execute(userData.email, userData.password);
+      case Failure(:final exception):
+        return Failure(ServiceException(message: exception.message));
+    } 
+  }
+}
