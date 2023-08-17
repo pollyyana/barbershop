5f92a4bf8ca308107645399fc494ef8b3d783c9f
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-17 15:56:31 -0300

    criar tela criar conta de usuario

--- barbershop/lib/src/barbershop_app.dart
+++ barbershop/lib/src/barbershop_app.dart
@@ -3,6 +3,7 @@ import 'package:barbershop/src/core/ui/barbershop_nav_global_key.dart';
 import 'package:barbershop/src/core/ui/barbershop_theme.dart';
 import 'package:barbershop/src/core/ui/widgets/barbershop_loader.dart';
 import 'package:barbershop/src/features/auth/login/login_page.dart';
+import 'package:barbershop/src/features/auth/register/user_register_page.dart';
 import 'package:flutter/material.dart';
 
 import 'features/splash/splash_page.dart';
@@ -23,6 +24,7 @@ class BarbershopApp extends StatelessWidget {
           routes: {
             '/': (_) => const SplashPage(),
             '/auth/login': (_) => const LoginPage(),
+            '/auth/register/user': (_) => const UserRegisterPage(),
             '/home/adm': (_) => const Text('ADM'),
             '/home/employee': (_) => const Text('Employee'),
           },
---
+++ barbershop/lib/src/features/auth/register/user_register_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class UserRegisterPage extends StatelessWidget {
+  const UserRegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Criar Conta',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Nome')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('E-mail')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text('Senha')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              TextFormField(
+                decoration:
+                    const InputDecoration(label: Text('Confirmar Senha')),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(56),
+                ),
+                onPressed: () {},
+                child: const Text('CRIAR CONTA'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
