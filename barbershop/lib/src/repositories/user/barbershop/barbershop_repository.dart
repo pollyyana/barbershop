8c84e5cc110fd46e96905a1462aad0fb0c09e048
pollyyana <pollyana.medeirosk@gmail.com>
2023-08-18 18:07:49 -0300

    implementaçao repository tela cria conta

--- barbershop/lib/src/repositories/user/barbershop/barbershop_repository.dart
+++ barbershop/lib/src/repositories/user/barbershop/barbershop_repository.dart
@@ -1,10 +1,17 @@
 import 'package:barbershop/src/core/exceptions/repository_exception.dart';
 import 'package:barbershop/src/core/fp/either.dart';
+import 'package:barbershop/src/core/fp/nil.dart';
 import 'package:barbershop/src/model/barbershop_model.dart';
 import 'package:barbershop/src/model/user_model.dart';
 
 abstract interface class BarbershopRepository {
+
+  Future<Either<RepositoryException, Nil>> save(({
+    String name ,
+    String email ,
+    List<String>openingDays,
+    List<String>openingHours,
+  })data);
   Future<Either<RepositoryException, BarbershopModel>> getMyBarbershop(
       UserModel userModel);
 }
-//sera que o erro nao esta em outro arquivo?
--- barbershop/lib/src/repositories/user/barbershop/barbershop_repository_impl.dart
+++ barbershop/lib/src/repositories/user/barbershop/barbershop_repository_impl.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:barbershop/src/core/exceptions/repository_exception.dart';
 import 'package:barbershop/src/core/fp/either.dart';
+import 'package:barbershop/src/core/fp/nil.dart';
 import 'package:barbershop/src/core/restClient/rest_client.dart';
 import 'package:barbershop/src/model/barbershop_model.dart';
 import 'package:barbershop/src/model/user_model.dart';
@@ -14,7 +17,6 @@ class BarbershopRepositoryImpl implements BarbershopRepository {
     required this.restClient,
   });
 
-  
   @override
   Future<Either<RepositoryException, BarbershopModel>> getMyBarbershop(
       UserModel userModel) async {
@@ -26,12 +28,35 @@ class BarbershopRepositoryImpl implements BarbershopRepository {
         );
         return Success(BarbershopModel.fromMap(data));
 
-     case UserModelEmployee():
+      case UserModelEmployee():
         final Response(:data) =
-            await restClient.auth.get(
-              '/barbershop/${userModel.barbershopId}'
-            );
+            await restClient.auth.get('/barbershop/${userModel.barbershopId}');
         return Success(BarbershopModel.fromMap(data));
-        }
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, Nil>> save(
+      ({
+        String email,
+        String name,
+        List<String> openingDays,
+        List<String> openingHours,
+      }) data) async {
+    try {
+      await restClient.auth.post('/barbershop', data: {
+        'user_id': '#userAuthRef',
+        'name': data.name,
+        'email': data.email,
+        'opening_days': data.openingDays,
+        'opening_hours': data.openingHours,
+      });
+      return Success(nil);
+    } on DioException catch (e, s) {
+      log('Erro ao registrar barbearia', error: e, stackTrace: s);
+      return Failure(
+        RepositoryException(message: 'Erro ao registrar barbearia'),
+      );
+    }
   }
 }
